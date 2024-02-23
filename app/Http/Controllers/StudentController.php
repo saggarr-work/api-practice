4041cfb763e83ddd0bab0d1cc95825e04f8c50ce
avijit biswas sagar <saggarr.work@gmail.com>
2024-02-23 10:08:30 +0600

    api done get, post, put, delete

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,8 +4,70 @@ namespace App\Http\Controllers;
 
 use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
-    
+    public function index(){
+        $student = Student::all();
+        $data = [
+            'status'    => 200,
+            'student'   => $student
+        ];
+        return response()->json($data, 200);
+    }
+
+    public function store(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name'      => 'required',
+            'email'     => 'required|email|unique:students,email',
+            'mobile'    => 'numeric'
+        ]);
+        if($validator->fails()){
+            $data = [
+                'status'    => 422,
+                'message'   => $validator->messages()
+            ];
+            return response()->json($data, 422);
+        }else{
+            Student::newStudent($request);
+            $data = [
+                'status'    => 200,
+                'message'   => 'data added successfully'
+            ];
+            return response()->json($data, 200);
+        }
+    }
+
+    public function edit(Request $request, String $id){
+        $validator = Validator::make($request->all(), [
+            'name'      => 'required',
+            'email'     => 'required|email',
+            'mobile'    => 'numeric'
+        ]);
+        if($validator->fails()){
+            $data = [
+                'status'    => 422,
+                'message'   => $validator->messages()
+            ];
+            return response()->json($data, 422);
+        }else{
+            Student::updateStudent($request, $id);
+            $data = [
+                'status'    => 200,
+                'message'   => 'data updated successfully'
+            ];
+            return response()->json($data, 200);
+        }
+    }
+
+    public function delete(String $id){
+        Student::deleteStudent($id);
+        $data = [
+            'status'    => 200,
+            'message'   => 'data deleted successfully'
+        ];
+        return response()->json($data, 200);
+    }
+
 }
--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+    private static $student;
+
+    public static function newStudent($request){
+        self::$student = new Student();
+        self::$student->name = $request->name;
+        self::$student->email = $request->email;
+        self::$student->mobile = $request->mobile;
+        self::$student->save();
+    }
+
+    public static function updateStudent($request, $id){
+        self::$student = Student::find($id);
+        self::$student->name = $request->name;
+        self::$student->email = $request->email;
+        self::$student->mobile = $request->mobile;
+        self::$student->save();
+    }
+
+    public static function deleteStudent($id){
+        self::$student = Student::find($id);
+        self::$student->delete();
+    }
 }
